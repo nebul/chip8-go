1990bf7e1afe335b916455f730275636d9f779b2
Ju <nebul@users.noreply.github.com>
2023-11-03 00:49:14 +0100

    fix some issues

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
-import {	
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+import (
 	"image"
 	"image/color"
 	"image/draw"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 )
 
 func main() {
@@ -17,43 +18,43 @@ func main() {
 	clock := NewFixedClock()
 
 	application := app.New()
-	windows := application.NewWindow("Chip-8 Emulator")
-	windows.Resize(fyne.NewSize(640, 320))
+	window := application.NewWindow("Chip-8 Emulator")
+	window.Resize(fyne.NewSize(640, 320))
 
 	displayCanvas := canvas.NewRaster(func(w, h int) image.Image {
 		pixel := image.NewNRGBA(image.Rect(0, 0, 64*10, 32*10))
 		for y := 0; y < 32; y++ {
 			for x := 0; x < 64; x++ {
 				column := color.White
-				if chip8Core.GetPixel(x,y) {
+				if chip8Core.GetPixel(uint8(x), uint8(y)) {
 					column = color.Black
 				}
-				draw.Draw(pixel, image.Rect(x*10, y*10, (x+1)*10, (y+1)*10), &image.Uniform{col}, image.Point{}, draw.Src)
+				draw.Draw(pixel, image.Rect(x*10, y*10, (x+1)*10, (y+1)*10), &image.Uniform{column}, image.Point{}, draw.Src)
 			}
 		}
 		return pixel
 	})
 
-	window.SetContent(container.NewWithoutLayout(displayCanvas)) 
+	window.SetContent(container.NewWithoutLayout(displayCanvas))
 
-	windows.Canvas().SetOnTypedKey(func(key *fyne.KeyEvent) {
+	window.Canvas().SetOnTypedKey(func(key *fyne.KeyEvent) {
 		var keyMap = map[fyne.KeyName]byte{
-			fyne.KeyX:      0x0,
-			fyne.Key1:      0x1,
-			fyne.Key2:      0x2,
-			fyne.Key3:      0x3,
-			fyne.KeyQ:      0x4,
-			fyne.KeyW:      0x5,
-			fyne.KeyE:      0x6,
-			fyne.KeyA:      0x7,
-			fyne.KeyS:      0x8,
-			fyne.KeyD:      0x9,
-			fyne.KeyZ:      0xA,
-			fyne.KeyC:      0xB,
-			fyne.Key4:      0xC,
-			fyne.KeyR:      0xD,
-			fyne.KeyF:      0xE,
-			fyne.KeyV:      0xF,
+			fyne.KeyX: 0x0,
+			fyne.Key1: 0x1,
+			fyne.Key2: 0x2,
+			fyne.Key3: 0x3,
+			fyne.KeyQ: 0x4,
+			fyne.KeyW: 0x5,
+			fyne.KeyE: 0x6,
+			fyne.KeyA: 0x7,
+			fyne.KeyS: 0x8,
+			fyne.KeyD: 0x9,
+			fyne.KeyZ: 0xA,
+			fyne.KeyC: 0xB,
+			fyne.Key4: 0xC,
+			fyne.KeyR: 0xD,
+			fyne.KeyF: 0xE,
+			fyne.KeyV: 0xF,
 		}
 		if chipKey, exists := keyMap[key.Name]; exists {
 			chip8Core.SetKey(chipKey, true)
@@ -61,7 +62,7 @@ func main() {
 	})
 
 	updateDisplay := func() {
-		displayCanvas.Refresh()
+		window.Canvas().Refresh(displayCanvas)
 	}
 
 	chip8Core.LoadROM([]byte("roms/PONG"))
@@ -84,7 +85,7 @@ func main() {
 		}
 	}()
 
-	windows.ShowAndRun()
+	window.ShowAndRun()
 
 	chip8Core.Stop()
 	clock.Stop()
